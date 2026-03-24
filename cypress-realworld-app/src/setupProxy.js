59f3c261c3b17bbb077cafdcc0f82a3d24824f79
Arande Souza <arande89@gmail.com>
2026-03-24 20:05:38 -0300

    ajustar yml

--- cypress-realworld-app/src/setupProxy.js
+++ cypress-realworld-app/src/setupProxy.js
@@ -2,9 +2,11 @@ const createProxyMiddleware = require("http-proxy-middleware");
 require("dotenv").config();
 
 module.exports = function (app) {
+  const backendPort = process.env.VITE_BACKEND_PORT || process.env.BACKEND_PORT || "3001";
+
   app.use(
     createProxyMiddleware(["/login", "/callback", "/logout", "/checkAuth", "graphql"], {
-      target: `http://localhost:${process.env.BACKEND_PORT}`,
+      target: `http://localhost:${backendPort}`,
       changeOrigin: true,
       logLevel: "debug",
     })
